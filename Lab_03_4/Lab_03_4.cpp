776bb60967553f85e70137777f85b31d82dc20fb
zasidkayuliia <yuliia.zasidka.ri.2024@lpnu.ua>
2024-09-24 00:56:03 +0300

    commit

---
+++ Lab_03_4/Lab_03_4.cpp
@@ -0,0 +1,25 @@
+﻿// Lab_03_4.cpp
+// < Засідка Юлія >
+// Лабораторна робота 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 10
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x, y, a, b, R;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "R = "; cin >> R;
+	// розгалуження в повній формі
+	
+	if (-R <= x &&  x <= 0 && -R <= y && y <=0 && x*x + y*y <= R*R||
+		(x <= a && 0 <= y &&  y <= b && x*x + y*y >= R*R))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
